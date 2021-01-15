f51b23bb8a6755f4969d1377bca39aba088b6dfe
Dipendra-creator <66115431+Dipendra-creator@users.noreply.github.com>
2021-01-15 11:11:39 +0530

    Initial commit

---
+++ CodeForces/A_Watermelon.cpp
@@ -0,0 +1,28 @@
+// https://codeforces.com/problemset/problem/4/A
+// Watermelon
+// A2OJ
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define double long double
+#define int long long
+#define endl "\n"
+
+using namespace std;
+
+signed main()
+{
+    fastio
+
+    int a;
+    cin >> a;
+    if (a == 2)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << ((a & 1) ? "NO" : "YES");
+    }
+    
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Competitive-Programing
+ 
