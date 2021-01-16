b54c11e39d3dbe96ad50fad787b8f954bec2dd90
Dipendra-creator <66115431+Dipendra-creator@users.noreply.github.com>
2021-01-16 12:03:16 +0530

    ✅Solved Twins✅

--- CodeForces/A_Twins.cpp
+++ CodeForces/A_Twins.cpp
@@ -13,28 +13,43 @@ signed main()
 {
 	fastio
 
+    // defining sum variables
 	int n, sum = 0, sum1 = 0, count;
 
+    // taking input of total numbers
     cin >> n;
 
+    // creating Empty array
     int arr[n];
 
+    // taking input of array and its all element sum
     for (int i = 0; i < n; i++){
         cin >> arr[i];
-        sum += arr[i];
+        sum += arr[i]; // sum of each element in array
     }
 
+    // sorting of array
     sort(arr, arr + n);
+    
+    // reversing of array
     reverse(arr, arr + n);
 
+    // defining counter as 1 
     count = 1;
+
+    // logic
     for (int i = 0; i < n; i++){
+
+        // sum of element those he already taken
         sum1 += arr[i];
+
+        // creating counter of coin picked
         if(sum1 <= sum - sum1){
             count++;
         }
     }
 
+    // counter printing
     cout << count;
     
     return 0;
