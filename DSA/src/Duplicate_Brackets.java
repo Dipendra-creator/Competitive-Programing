aeb916c1aaea7371bba14cc4913977a191510b42
Dipendra-creator <66115431+Dipendra-creator@users.noreply.github.com>
2021-01-28 15:54:45 +0530

    ✅Solved Duplicate Brackets✅

---
+++ DSA/src/Duplicate_Brackets.java
@@ -0,0 +1,2 @@
+public class Duplicate_Brackets {
+}
--- Testing/main.cpp
+++ Testing/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    string firstName;
-    cout << "Type your first name: ";
-    cin >> firstName; // get user input from the keyboard
-    cout << "Your name is: " << firstName;
+
+    int x=12,y=11;
+    int c = (1100)&(1011);
+    // double a=4 ,b = 5;
+    // double e = 4.0/5;
+    cout << c;
     return 0;
 } 
