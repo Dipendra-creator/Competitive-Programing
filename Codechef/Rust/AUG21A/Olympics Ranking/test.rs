88b319f9855800ea92d6f38878280c4202051495
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2021-08-08 13:32:29 +0530

    CodeChef

---
+++ Codechef/Rust/AUG21A/Olympics Ranking/test.rs
@@ -0,0 +1,45 @@
+fn int_input() -> i32 {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    let n: i32 = line.trim().parse().expect("invalid input");
+    return n;
+}
+
+fn int_space_input() -> Vec<i32> {
+    let mut vec0 = Vec::new();
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).expect("input");
+    let nums = line.trim().split(' ').flat_map(str::parse::<i32>).collect::<Vec<_>>();
+    for num in nums {
+        vec0.push(num);
+    }
+    return vec0;
+}
+
+fn sum_array(array: Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for i in 0..array.len() {
+        if i < 3 {
+            sum = sum + array[i];
+        }
+        else {
+            sum = sum - array[i]
+        }        
+    }
+    return sum;
+}
+
+
+fn main() {
+    let t: i32 = int_input();
+    for _l in 0..t {
+        let array = int_space_input();
+        if sum_array(array) > 0 {
+            println!("{}", '1');
+        }
+        else {
+            println!("{}", '2');
+        }
+    }
+    
+}
---
+++ Codechef/Rust/AUG21A/Problem Difficulties/main.rs
@@ -0,0 +1,52 @@
+use std::collections::HashSet;
+
+fn int_input() -> i32 {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    let n: i32 = line.trim().parse().expect("invalid input");
+    return n;
+}
+
+fn int_space_input() -> Vec<i32> {
+    let mut vec0 = Vec::new();
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).expect("input");
+    let nums = line.trim().split(' ').flat_map(str::parse::<i32>).collect::<Vec<_>>();
+    for num in nums {
+        vec0.push(num);
+    }
+    
+    return vec0;
+}
+
+
+fn main() {
+    let t: i32 = int_input();
+
+    for _i in 0..t {
+        let array = int_space_input();
+
+        let hash_set: HashSet<i32> = array.iter().cloned().collect();
+
+        let final_array: Vec<&i32> = hash_set.iter().collect();
+
+        let len_of_set = final_array.len();
+
+        if len_of_set == 4 {
+            println!("{}", '2');
+        }
+        else if len_of_set == 3 {
+            println!("{}", '1');
+        }
+        else if len_of_set == 2 {
+            println!("{}", '1');
+        }
+        else if len_of_set == 1 {
+            println!("{}", '0');
+        }
+        else {
+            println!("{}", '0');
+        }
+        
+    }
+}
