346ed489838162c0803771439c08d79f9e10c934
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2021-08-09 10:55:40 +0530

    Rust

--- Codechef/Python/test.py
+++ Codechef/Python/test.py
@@ -1,16 +1,27 @@
-T = int(input())
-for i in range(T):
-    l = set(list(map(int, input().split())))
-    if len(l) == 4:
-        print(2)
-    elif len(l) == 3:
-        print(2)
-    elif len(l) == 2:
-        l.sort()
-        b = l[0]
-        if l.count(b) == 2:
-            print(2)
+try:
+    for i in range(1):
+        n,p,k= 10, 5, 5
+        n= int(n)
+        p=int(p)
+        k=int(k)
+        ans=0
+        a=p%k - 1
+        print(p,"%" , k-1, "=", a)
+        b=((n-1)//k)*k
+        print("(", (n-1), "//", k, ")*", k, "=", b)
+
+        print(n,"-",1,"-",b,"=",n-1-b)
+        b=n-1-b
+        if (a>b):
+            ans+=(b+1)*((n-1)//k + 1)+ (a-b)*((n-1)//k)
         else:
-            print(1)
-    else:
-        print(0)
+            print("((",n-1,")//",k + 1,")*(",a+1,")=", ((n-1)//k + 1)*(a+1))
+            print(ans, "+=", ((n-1)//k + 1)*(a+1))
+            ans+=((n-1)//k + 1)*(a+1)
+        for i in range(a+1,n,k):
+            ans+=1
+            if(i==p):
+                print(ans)
+                break
+except:
+    pass
--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,26 +1,27 @@
-fn int_input() -> i32 {
+fn int_input() -> i64 {
     let mut line = String::new();
     std::io::stdin().read_line(&mut line).unwrap();
-    let n: i32 = line.trim().parse().expect("invalid input");
+    let n: i64 = line.trim().parse().expect("invalid input");
     return n;
 }
 
 
 fn main() {
-    let t: i32 = int_input();
-    
+    let t: i64 = int_input();
+
     for _e in 0..t {
-        let n = int_input();
+        let n: i64 = int_input();
         let mut count = 0;
         for a in 1..n+1 {
             let mut b = a;
             while b <= n {
+                println!("for (int b={}; b<= {}; b += {})", a, n, a);
                 if b%a == 0 {
                     let mut c = a;
                     while c <= n {
                         if c%b == a {
                             count += 1
-                        } 
+                        }
                         c += b;
                     }
                 }
